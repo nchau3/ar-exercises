a148f79547349f711c32240b48e40dc3f28f7d37
Nicholas <nc_chau@hotmail.com>
2022-10-20 01:04:06 -0400

    complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,8 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+store_name = gets.chomp
+errors = Store.create(name: store_name).errors[:name]
+errors.each do |error|
+  puts error
+end
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,6 @@
 class Employee < ActiveRecord::Base
+  validates :store_id, :first_name, :last_name, :hourly_rate, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200}
+  
   belongs_to :store
 end
